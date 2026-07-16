5b48c2b4ae1f4d59067f9093af04862f9d2fdfaa
Pablo-M-Santos <pablomoreirasantos.hp@gmail.com>
2026-07-16 15:12:23 -0300

    feat: add Department entity with attributes and timestamps

---
+++ backend/src/main/java/br/com/logicore/modules/department/entity/Department.java
@@ -0,0 +1,42 @@
+package br.com.logicore.modules.department.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "area_department")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String nome;
+
+    @Column(length = 500)
+    private String descricao;
+
+    @Column(length = 20)
+    private String sigla;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean ativo = true;
+
+    @CreationTimestamp
+    @Column(name = "criado_em", updatable = false)
+    private LocalDateTime criadoEm;
+
+    @UpdateTimestamp
+    @Column(name = "atualizado_em")
+    private LocalDateTime atualizadoEm;
+}
