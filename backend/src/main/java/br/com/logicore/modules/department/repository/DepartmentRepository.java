5e7abfd710cedc3a32317f4157900c07aee67db7
Pablo-M-Santos <pablomoreirasantos.hp@gmail.com>
2026-07-16 15:13:24 -0300

    feat: add DepartmentRepository for managing Department entities

---
+++ backend/src/main/java/br/com/logicore/modules/department/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package br.com.logicore.modules.department.repository;
+
+import br.com.logicore.modules.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    boolean existsByNome(String nome);
+    boolean existsByNomeAndIdNot(String nome, Long id);
+}
