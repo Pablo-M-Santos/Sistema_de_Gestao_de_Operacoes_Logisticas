be329c4d04103739a02ba7e245f5a5602f6bfacc
Pablo-M-Santos <pablomoreirasantos.hp@gmail.com>
2026-07-17 13:34:01 -0300

    feat: update pom.xml and add SQL script for cargo table creation

---
+++ backend/src/main/java/br/com/logicore/modules/cargo/entity/Cargo.java
@@ -0,0 +1,45 @@
+package br.com.logicore.modules.cargo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cargo")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cargo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String nome;
+
+    @Column(length = 500)
+    private String descricao;
+
+    @Column(unique = true, length = 20)
+    private String codigo;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean ativo = true;
+
+    @CreationTimestamp
+    @Column(name = "criado_em", nullable = false, updatable = false)
+    private LocalDateTime criadoEm;
+
+    @UpdateTimestamp
+    @Column(name = "atualizado_em")
+    private LocalDateTime atualizadoEm;
+
+}
