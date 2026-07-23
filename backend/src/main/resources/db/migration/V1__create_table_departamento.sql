4886a8218773c597a55e232966b9d8c4b23bb6fa
Pablo-M-Santos <pablomoreirasantos.hp@gmail.com>
2026-07-23 16:41:38 -0300

    refactor: rename table and adjust column sizes for department table

--- backend/src/main/resources/db/migration/V1__create_table_departamento.sql
+++ backend/src/main/resources/db/migration/V1__create_table_departamento.sql
@@ -1,9 +1,9 @@
 CREATE TABLE department
 (
     id            BIGSERIAL PRIMARY KEY,
-    nome          VARCHAR(150)                             NOT NULL UNIQUE,
-    descricao     VARCHAR(500),
-    sigla         VARCHAR(20),
+    nome          VARCHAR(80)                             NOT NULL UNIQUE,
+    descricao     VARCHAR(250),
+    sigla         VARCHAR(10),
     status        VARCHAR(20) NOT NULL DEFAULT 'ACTIVE',
     criado_em     TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     atualizado_em TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
