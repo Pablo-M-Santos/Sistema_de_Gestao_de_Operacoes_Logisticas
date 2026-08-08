df643960817247005f871ea049c2bb1023edb716
Pablo-M-Santos <pablomoreirasantos.hp@gmail.com>
2026-08-07 21:51:30 -0300

    feat: annotate CargoRepository with @Repository for improved Spring component scanning

--- backend/src/main/java/br/com/logicore/modules/cargo/repository/CargoRepository.java
+++ backend/src/main/java/br/com/logicore/modules/cargo/repository/CargoRepository.java
@@ -3,9 +3,11 @@ package br.com.logicore.modules.cargo.repository;
 import br.com.logicore.modules.cargo.entity.Cargo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CargoRepository extends JpaRepository<Cargo, Long>,
         JpaSpecificationExecutor<Cargo> {
 
